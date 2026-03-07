d1e6e70a20f25a3bcf1526d40cb47f8eccccc752
Emilian Scheel <emilian.scheel@gmail.com>
2026-03-07 08:34:38 +0100

    init

---
+++ minimal-map.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Minimal Map
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     minimal-map
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Minimal_Map
+ */
+
+// Your code starts here.
